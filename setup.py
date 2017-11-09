99f7310a5b8dbf9721d6a895f4a759a1bc4c9b67
Dan Black <dyspop@gmail.com>
2017-11-09 15:13:26 -0500

    the start, a joke

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='responsysrest',
+      version='0.1',
+      description='Python client library for the Responsys Interact REST API',
+      url='http://github.com/dyspop/responsysrest',
+      author='Dan Black',
+      author_email='dyspop@gmail.com',
+      license='MIT',
+      packages=['responsysrest'],
+      zip_safe=False)
