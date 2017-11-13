818d2eee523cee2b196a6956a30bb7db97b29f27
Dan Black <dyspop@gmail.com>
2017-11-13 09:03:58 -0500

    move code to client

---
+++ responsysrest/client.py
@@ -0,0 +1,21 @@
+# container for context
+secrets = {"user_name": "", "password": ""}
+
+import requests
+from secret import secrets
+
+print(secrets)
+
+headers = {'content-type': 'application/x-www-form-urlencoded'}
+base_url = 'http://login5.responsys.net/rest/api/v1.3/'
+
+def login_with_username_and_password(url, user_name, password):
+    service_url = 'auth/token'
+    data = {
+        "user_name" : user_name,
+        "password" : password,
+        "auth_type" : "password"
+    }
+    url = url + service_url
+    return requests.post(url, data=data, headers=headers)
+    
