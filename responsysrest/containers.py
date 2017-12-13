dbf56e263e427cbd6429d98ff1aab00a18d81ad7
Dan Black <dyspop@gmail.com>
2017-12-13 09:19:36 -0500

    linted

--- responsysrest/containers.py
+++ responsysrest/containers.py
@@ -1,29 +1,33 @@
 # TODO expose as a class with enums
 rules = {
-    'merge_or_update_members_in_a_profile_list_table' :
+    'merge_or_update_members_in_a_profile_list_table':
         [
             {
-                'recordData' : {
-                    'fieldNames' : [
+                'recordData': {
+                    'fieldNames': [
                         'email_address_'
+                        '''
                         #'riid_',
                         #'mobile_number_',
                         #'email_address_'
+                        '''
                     ],
-                    'records' : [
+                    'records': [
                         [
                             'a@b.c'
+                            '''
                             #'0123456', # RIID_
-                            #'0123456789', # U.S. Ten-digit format phone number without dashes
+                            #'0123456789', # US Ten-digit phone w/o dashes
                             #'a@b.c' # email address
+                            '''
                         ]
                     ],
-                    'mergeTemplateName' : None
+                    'mergeTemplateName': None
                 },
-                'mergeRule' : {
-                    'htmlValue' : {
-                        'default' : 'H',
-                        'options' : [
+                'mergeRule': {
+                    'htmlValue': {
+                        'default': 'H',
+                        'options': [
                             'H',
                             'h',
                             'HTML',
@@ -34,17 +38,17 @@ rules = {
                             'Htm'
                         ]
                     },
-                    'optinValue' : {
-                        'default' : 'I',
-                        'options' : [
+                    'optinValue': {
+                        'default': 'I',
+                        'options': [
                             'I',
                             'i',
                             '1'
                         ]
                     },
-                    'textValue' : {
-                        'default' : 'T',
-                        'options' : [
+                    'textValue': {
+                        'default': 'T',
+                        'options': [
                             'T',
                             't',
                             'TEXT',
@@ -55,86 +59,101 @@ rules = {
                             'Txt'
                         ]
                     },
-                    'insertOnNoMatch' : {
-                        'default' : True,
-                        'options' : [
+                    'insertOnNoMatch': {
+                        'default': True,
+                        'options': [
                             True,
                             False
                         ]
                     },
-                    'updateOnMatch' : {
-                        'default' : 'REPLACE_ALL',
-                        'options' : [
+                    'updateOnMatch': {
+                        'default': 'REPLACE_ALL',
+                        'options': [
                             'NO_UPDATE',
                             'REPLACE_ALL'
                         ]
                     },
-                    'matchColumnName1' : {
-                        'default' : 'RIID_',
-                        'options' : [
+                    'matchColumnName1': {
+                        'default': 'RIID_',
+                        'options': [
                             'RIID_',
                             'CUSTOMER_ID_',
                             'EMAIL_ADDRESS_',
                             'MOBILE_NUMBER_',
-                            'EMAIL_MD5_HASH_', # this shouldn't be an option!
+                            'EMAIL_MD5_HASH_',
                             'EMAIL_SHA256_HASH_'
                         ]
                     },
-                    'matchColumnName2' : {
-                        'default' : None,
-                        'options' : [
+                    'matchColumnName2': {
+                        'default': None,
+                        'options': [
                             None,
                             'RIID_',
                             'CUSTOMER_ID_',
                             'EMAIL_ADDRESS_',
                             'MOBILE_NUMBER_',
-                            'EMAIL_MD5_HASH_', # this shouldn't be an option!
+                            'EMAIL_MD5_HASH_',
                             'EMAIL_SHA256_HASH_'
                         ]
                     },
-                    # 'matchColumnName3' : {
-                    #     'default' : None,
-                    #     'options' : [
+                    '''
+                    # 'matchColumnName3': {
+                    #     'default': None,
+                    #     'options': [
                     #         None
                     #     ] # deprecated
                     # },
-                    'matchOperator' : {
-                        'default' : 'NONE',
-                        'options' : [
+                    '''
+                    'matchOperator': {
+                        'default': 'NONE',
+                        'options': [
                             'NONE',
                             'AND'
                         ]
                     },
-                    'optoutValue' : {
-                        'default' : 'O',
-                        'options' : [
+                    'optoutValue': {
+                        'default': 'O',
+                        'options': [
                             'O',
                             'o',
                             '0'
                         ]
                     },
-                    'rejectRecordIfChannelEmpty' : {
-                        'default' : None,
-                        'options' : [
+                    'rejectRecordIfChannelEmpty': {
+                        'default': None,
+                        'options': [
                             None,
-                            'E', # Email
-                            'M', # Mobile
-                            'P', # Postal address
-                            'E,M', # Email or Mobile
-                            'E,P', # Email or Postal
-                            'M,E', # Mobile or Email
-                            'M,P', # Mobile or Postal
-                            'E,M,P', # Email, Mobile or Postal
-                            'E,P,M', # Email, Postal or Mobile
-                            'M,E,P', # Mobile, Email or Postal
-                            'M,P,E', # Mobile, Postal or Email
-                            'P,E,M', # Postal, Email or Postal
-                            'P,M,E' # Postal, Mobile or Email
+                            # Email
+                            'E',
+                            # Mobile
+                            'M',
+                            # Postal address
+                            'P',
+                            # Email or Mobile
+                            'E,M',
+                            # Email or Postal
+                            'E,P',
+                            # Mobile or Email
+                            'M,E',
+                            # Mobile or Postal
+                            'M,P',
+                            # Email, Mobile or Postal
+                            'E,M,P',
+                            # Email, Postal or Mobile
+                            'E,P,M',
+                            # Mobile, Email or Postal
+                            'M,E,P',
+                            # Mobile, Postal or Email
+                            'M,P,E',
+                            # Postal, Email or Postal
+                            'P,E,M',
+                            # Postal, Mobile or Email
+                            'P,M,E'
                         ]
                     },
-                    'defaultPermissionStatus' : {
-                        'default' : 'OPTIN',
-                        'options' : [
+                    'defaultPermissionStatus': {
+                        'default': 'OPTIN',
+                        'options': [
                             'OPTIN',
                             'OPTOUT'
                         ]
@@ -142,10 +161,6 @@ rules = {
                 }
             }
         ],
-        'query_attributes_allowed' : [
-            'r', # RIID
-            'e', # EMAIL_ADDRESS
-            'c', # CUSTOMER_ID
-            'm'  # MOBILE_NUMBER
-        ]
-    }
+        # RIID, EMAIL_ADDRESS, CUSTOMER_ID, MOBILE_NUMBER
+    'query_attributes_allowed': ['r', 'e', 'c', 'm']
+}
