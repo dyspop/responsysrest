d9b2d7d2c32fc2d47e01fb4124657b44a8331975
Dan Black <dyspop@gmail.com>
2017-12-13 14:22:07 -0500

    first tests

---
+++ tests/test_client.py
@@ -0,0 +1,17 @@
+"""Tests for each individual function in the Client."""
+import responsysrest as r
+
+# import with two names for grammar purposes.
+from secret import secrets as secret, secrets
+
+
+def test_secrets():
+    """Test to see if our secrets are stored."""
+    for key in secrets:
+        assert secret[key] is not None
+        assert secret[key] != ''
+
+
+def test_profile_lists():
+    """Test to see if profile lists has data."""
+    assert len(r.profile_lists()) > 0
