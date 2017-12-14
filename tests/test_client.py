cd9b2b7daffe3f48bb26ccad4a6bb4a4abf0e5be
Dan Black <dyspop@gmail.com>
2017-12-14 16:26:55 -0500

    update tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,15 +1,23 @@
 """Tests for each individual function in the Client."""
 import responsysrest as r
 
-# import with two names for grammar purposes.
-from secret import secrets as secret, secrets
+context = r.get_context()
 
 
-def test_secrets():
-    """Test to see if our secrets are stored."""
-    for key in secrets:
-        assert secret[key] is not None
-        assert secret[key] != ''
+def test_does_get_context_get_auth():
+    """Test if get_context returns an authToken."""
+    assert None is not context['authToken']
+    assert '' != context['authToken']
+
+
+def test_does_get_context_get_endpoint():
+    """Test if get_context return an https endpoint."""
+    assert None is not context['endPoint']
+    assert '' != context['endPoint']
+    before, https, after = context['endPoint'].rpartition('https://')
+    assert '' == before
+    assert 'https://' == https
+    assert 'responsys.' in after
 
 
 def test_profile_lists():
