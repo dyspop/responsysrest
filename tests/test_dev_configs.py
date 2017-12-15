1cfab8de1c1c609b360e9a1c4e985d12c24b73b3
Dan Black <dyspop@gmail.com>
2017-12-15 16:10:40 -0500

    run this first

--- tests/test_dev_configs.py
+++
@@ -1,9 +0,0 @@
-"""Test development configuration."""
-from secret import secrets as secret
-
-
-def test_import_secrets():
-    """Test everything the secrets, the user may not have added them."""
-    for k in secret:
-        assert None is not secret[k]
-        assert '' != secret[k]
