07593627f55551185bc3e165b8b4168302605669
Dan Black <dyspop@gmail.com>
2017-12-15 13:55:08 -0500

    don't delete the fixture riid in ri'

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,7 +4,7 @@ import responsysrest as r
 
 # Fixtures
 fixtures = {
-    'riid': '0123456789',
+    'riid': '12112123105',
     'profile_list': 'API_testing'
 }
 
@@ -37,14 +37,8 @@ def test_get_profile_lists_not_zero_length():
 def test_fixture_profile_list_in_get_profile_lists():
     """Now that we know there are profile lists, test if
     the fixture list is in Interact."""
-    profile_lists = [list['name'] for list in r.get_profile_lists()] 
+    profile_lists = [list['name'] for list in r.get_profile_lists()]
     assert fixtures['profile_list'] in profile_lists
-
-
-def test_get_campaigns_not_zero_length():
-    """Test to see if campaigns has data."""
-    # TODO: what happens if there are no lists defined in Interact?
-    assert len(r.get_campaigns()) > 0
 
 
 # def test_manage_profile_lists_returns_response():
@@ -60,6 +54,18 @@ def test_get_member_of_list_by_riid_returns_response():
     )
 
 
+def test_fixture_riid_in_fixture_profile_list():
+    assert 'recordData' in r.get_member_of_list_by_riid(
+        fixtures['profile_list'], fixtures['riid']
+    )
+
+
+def test_get_campaigns_not_zero_length():
+    """Test to see if campaigns has data."""
+    # TODO: what happens if there are no lists defined in Interact?
+    assert len(r.get_campaigns()) > 0
+
+
 def test_get_member_of_list_by_attribute_returns_response():
     """Test if the API responds."""
     assert None is not r.get_member_of_list_by_attribute(
@@ -73,8 +79,8 @@ def test_get_member_of_list_by_attribute_returns_response():
 def test_delete_from_profile_list_returns_response():
     """Test if the API responds."""
     assert None is not r.delete_from_profile_list(
-        fixtures['profile_list'], fixtures['riid']
+        fixtures['profile_list'], ''
     )
     assert '' != r.delete_from_profile_list(
-        fixtures['profile_list'], fixtures['riid']
+        fixtures['profile_list'], ''
     )
