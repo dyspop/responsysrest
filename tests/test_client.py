16a8b8699b4e161c3c1b1f57163b18804dd55c42
Dan Black <dyspop@gmail.com>
2017-12-15 16:13:05 -0500

    less verbose descriptive function name

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,7 +11,7 @@ context = r.get_context()
 
 
 # Test related functions.
-def _returns_not_none_nor_empty_str(func):
+def _heartbeat(func):
     """Instead of inspecting too many responses, we just test
     to see if there's something on the other end."""
     assert None is not func
@@ -22,13 +22,13 @@ def _returns_not_none_nor_empty_str(func):
 def test_get_context_returns_authtoken():
     """Test if get_context returns an authToken.
     Some responses do warrant inspection."""
-    _returns_not_none_nor_empty_str(context['authToken'])
+    _heartbeat(context['authToken'])
 
 
 def test_get_context_returns_endpoint():
     """Test if get_context returns a responsys https endpoint.
     Some responses do warrant inspection."""
-    _returns_not_none_nor_empty_str(context['endPoint'])
+    _heartbeat(context['endPoint'])
     before, https, after = context['endPoint'].rpartition('https://')
     assert '' == before
     assert 'https://' == https
@@ -53,7 +53,7 @@ def test_fixture_profile_list_in_get_profile_lists():
 
 def test_get_member_of_list_by_riid_returns_response():
     """Test if the API responds when we try to get a member."""
-    _returns_not_none_nor_empty_str(
+    _heartbeat(
         r.get_member_of_list_by_riid(
             fixtures['profile_list'], fixtures['riid']))
 
@@ -73,7 +73,7 @@ def test_get_campaigns_not_zero_length():
 def test_get_member_of_list_by_attribute_returns_response():
     """Test if the API responds when we get a member using
     the attribute feature."""
-    _returns_not_none_nor_empty_str(
+    _heartbeat(
         r.get_member_of_list_by_attribute(
             fixtures['profile_list'], fixtures['riid']))
 
@@ -81,18 +81,18 @@ def test_get_member_of_list_by_attribute_returns_response():
 def test_delete_from_profile_list_returns_response():
     """Test if the API responds when we try to delete a member
     from a list."""
-    _returns_not_none_nor_empty_str(
+    _heartbeat(
         r.delete_from_profile_list(fixtures['profile_list'], ''))
 
 
 def test_get_profile_extensions():
     """Test if the API responds when we try to get the profile
     extensions associated with a list."""
-    _returns_not_none_nor_empty_str(
+    _heartbeat(
         r.get_profile_extensions(fixtures['profile_list']))
 
 
 def test_create_profile_extension():
     """Test if the API responds when we try to create a profile extension."""
-    _returns_not_none_nor_empty_str(
+    _heartbeat(
         r.create_profile_extension(''))
