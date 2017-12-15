fc038756fc19016af19c76ef74258de2216835df
Dan Black <dyspop@gmail.com>
2017-12-15 09:19:34 -0500

    clean up grammar

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,14 +4,14 @@ import responsysrest as r
 context = r.get_context()
 
 
-def test_does_get_context_get_auth():
+def test_get_context_returns_authtoken():
     """Test if get_context returns an authToken."""
     assert None is not context['authToken']
     assert '' != context['authToken']
 
 
-def test_does_get_context_get_endpoint():
-    """Test if get_context return an https endpoint."""
+def test_get_context_returns_endpoint():
+    """Test if get_context returns a responsys https endpoint."""
     assert None is not context['endPoint']
     assert '' != context['endPoint']
     before, https, after = context['endPoint'].rpartition('https://')
