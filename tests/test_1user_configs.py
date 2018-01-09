5286fec3b577ec23c81927fe260dcb9098c905aa
Dan Black <dyspop@gmail.com>
2018-01-09 11:37:23 -0500

    add more user config tests

--- tests/test_1user_configs.py
+++ tests/test_1user_configs.py
@@ -4,6 +4,15 @@ import config
 
 def test_import_config():
     """Test everything the config, the user may not have added them."""
-    for k in config:
-        assert None is not config[k]
-        assert '' != config[k]
+    assert None is not config
+    assert '' != config
+
+
+def test_pod_exists():
+    """Test if we loaded something in the pod."""
+    assert config.pod
+
+
+def test_pod_is_number_like():
+    """Responsys Interact is only on one of two servers pods"""
+    assert str(config.pod) in ['2', '5']
