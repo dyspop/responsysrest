287cb6553ff7dc98165c72e8dde73e75b847b63c
Dan Black <dyspop@gmail.com>
2018-01-31 13:50:11 -0500

    improve user tests

--- tests/test_1user_configs.py
+++ tests/test_1user_configs.py
@@ -14,5 +14,41 @@ def test_pod_exists():
 
 
 def test_pod_is_number_like():
-    """Responsys Interact is only on one of two servers pods"""
-    assert str(config.pod) in ['2', '5']
+    """Interact is only on one of two servers pods."""
+    assert config.pod in ['2', '5']
+
+
+def test_api_folder():
+    """The API folder should be a non-zero length string."""
+    assert isinstance(config.api_folder, str)
+    assert 0 < len(config.api_folder)
+
+
+def test_api_list():
+    """The API list should be a non-zero length string."""
+    assert isinstance(config.api_list, str)
+    assert 0 < len(config.api_list)
+
+
+def test_profile_extension_table_alias():
+    """The profile extension table alias should be a non-zero length string."""
+    assert isinstance(config.profile_extension_table_alias, str)
+    assert 0 < len(config.profile_extension_table_alias)
+
+
+def test_supplemental_table_alias():
+    """The supplemental table alias should be a non-zero length string."""
+    assert isinstance(config.supplemental_table_alias, str)
+    assert 0 < len(config.supplemental_table_alias)
+
+
+def test_primary_key_alias():
+    """The primary key alias should be a non-zero length string."""
+    assert isinstance(config.primary_key_alias, str)
+    assert 0 < len(config.primary_key_alias)
+
+
+def test_riid_generator_length():
+    """The RIID generator length should be a non-zero length string."""
+    assert isinstance(config.riid_generator_length, int)
+    assert 0 < config.riid_generator_length
