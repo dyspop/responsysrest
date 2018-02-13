e205b243bfd670b1da5de3288af6b8e5183e9d6d
Dan Black <dyspop@gmail.com>
2018-02-13 10:59:41 -0500

    get/set class properties

--- responsysrest/client.py
+++ responsysrest/client.py
@@ -48,7 +48,7 @@ class Configuration:
 
     def __init__(
             self,
-            pod='',
+            pod='5',
             api_folder='___api-generated',
             api_list='API_testing',
             profile_extension_table_alias='_pet',
@@ -76,7 +76,7 @@ class Configuration:
         with open("config.json", "r") as f:
             raw = f.read()
             minified = jsmin(raw)
-            data = json.loads(minified)
+            data = json.loads(minified)[0]
 
     @property
     def pod(self):
@@ -102,12 +102,87 @@ class Configuration:
     @api_folder.setter
     def api_folder(self, api_folder):
         """Set the API folder."""
-        if isinstance(api_folder, str):
-            if 1 < len(api_folder) < 256:
-                self.__api_folder = api_folder
-            else:
-                raise ValueError('Must be shorter than 256 characters.')
+        self.__api_folder = api_folder
 
+    @property
+    def api_list(self):
+        """Get API list."""
+        return self.__api_list
+
+    @api_list.setter
+    def api_list(self, api_list):
+        """Set API list."""
+        self.__api_list = api_list
+
+    @property
+    def profile_extension_table_alias(self):
+        """Get profile extension table alias."""
+        return self.__profile_extension_table_alias
+
+    @profile_extension_table_alias.setter
+    def profile_extension_table_alias(self, profile_extension_table_alias):
+        """Set profile extension table alias."""
+        self.__profile_extension_table_alias = profile_extension_table_alias
+
+    @property
+    def supplemental_table_alias(self):
+        """Get supplemental table alias."""
+        return self.__supplemental_table_alias
+
+    @supplemental_table_alias.setter
+    def supplemental_table_alias(self, supplemental_table_alias):
+        """Set supplemental table alias."""
+        self.__supplemental_table_alias = supplemental_table_alias
+
+    @property
+    def primary_key_alias(self):
+        """Get primary key alias."""
+        return self.__primary_key_alias
+
+    @primary_key_alias.setter
+    def primary_key_alias(self, primary_key_alias):
+        """Set primary key alias."""
+        self.__primary_key_alias = primary_key_alias
+
+    @property
+    def riid_generator_length(self):
+        """Get riid generator length."""
+        return self.__riid_generator_length
+
+    @riid_generator_length.setter
+    def riid_generator_length(self, riid_generator_length):
+        """Set riid generator length."""
+        self.__riid_generator_length = riid_generator_length
+
+    @property
+    def test_email_address(self):
+        """Get test email address."""
+        return self.__test_email_address
+
+    @test_email_address.setter
+    def test_email_address(self, test_email_address):
+        """Set test email address."""
+        self.__test_email_address = test_email_address
+
+    @property
+    def test_campaign_name(self):
+        """Get test campaign name."""
+        return self.__test_campaign_name
+
+    @test_campaign_name.setter
+    def test_campaign_name(self, test_campaign_name):
+        """Set test campaign name."""
+        self.__test_campaign_name = test_campaign_name
+
+    @property
+    def test_content_library_folder(self):
+        """Get test content library folder."""
+        return self.__test_content_library_folder
+
+    @test_content_library_folder.setter
+    def test_content_library_folder(self, test_content_library_folder):
+        """Set test content library folder."""
+        self.__test_content_library_folder = test_content_library_folder
 
 
 
