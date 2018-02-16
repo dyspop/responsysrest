b030e332111ba56cb87a2c0817844e90de6ca1e6
Dan Black <dyspop@gmail.com>
2018-02-16 17:19:08 -0500

    call config from class

--- responsysrest/tests/test_1user_configs.py
+++ responsysrest/tests/test_1user_configs.py
@@ -4,65 +4,59 @@ import responsysrest as r
 
 def test_credentials_class():
     """Test that the class instantiates."""
-    assert r.config.Credentials()
-
+    assert r.Credentials('user', 'password', 'test@')
 
 def test_pod_exists():
     """Test if we loaded something in the pod."""
-    assert r.config.Interact().pod
+    assert r.Configuration().pod
 
 
 def test_pod_is_number_like():
     """Interact is only on one of two servers pods."""
-    assert r.config.Interact().pod in ['2', '5']
+    assert r.Configuration().pod in ['2', '5']
 
 
 def test_api_folder():
     """The API folder should be a non-zero length string."""
-    assert isinstance(r.config.Interact().api_folder, str)
-    assert 0 < len(r.config.Interact().api_folder)
+    assert isinstance(r.Configuration().api_folder, str)
+    assert 0 < len(r.Configuration().api_folder)
 
 
 def test_api_list():
     """The API list should be a non-zero length string."""
-    assert isinstance(r.config.Interact().api_list, str)
-    assert 0 < len(r.config.Interact().api_list)
+    assert isinstance(r.Configuration().api_list, str)
+    assert 0 < len(r.Configuration().api_list)
 
 
 def test_profile_extension_table_alias():
     """The profile extension table alias should be a non-zero length string."""
-    assert isinstance(r.config.Interact().profile_extension_table_alias, str)
-    assert 0 < len(r.config.Interact().profile_extension_table_alias)
+    assert isinstance(r.Configuration().profile_extension_table_alias, str)
+    assert 0 < len(r.Configuration().profile_extension_table_alias)
 
 
 def test_supplemental_table_alias():
     """The supplemental table alias should be a non-zero length string."""
-    assert isinstance(r.config.Interact().supplemental_table_alias, str)
-    assert 0 < len(r.config.Interact().supplemental_table_alias)
+    assert isinstance(r.Configuration().supplemental_table_alias, str)
+    assert 0 < len(r.Configuration().supplemental_table_alias)
 
 
 def test_primary_key_alias():
     """The primary key alias should be a non-zero length string."""
-    assert isinstance(r.config.Interact().primary_key_alias, str)
-    assert 0 < len(r.config.Interact().primary_key_alias)
+    assert isinstance(r.Configuration().primary_key_alias, str)
+    assert 0 < len(r.Configuration().primary_key_alias)
 
 
 def test_riid_generator_length():
     """The RIID generator length should be a non-zero length string."""
-    assert isinstance(r.config.Interact().riid_generator_length, int)
-    assert 0 < r.config.Interact().riid_generator_length
-
-
-def test_email_address_exists():
-    """Test if we loaded something in the test email address."""
-    assert r.config.Credentials().email_address
+    assert isinstance(r.Configuration().riid_generator_length, int)
+    assert 0 < r.Configuration().riid_generator_length
 
 
 def test_test_campaign_name_exists():
     """Test if we loaded something in the test campaign name."""
-    assert r.config.Interact().test_campaign_name
+    assert r.Configuration().test_campaign_name
 
 
 def test_test_content_library_folder_exists():
     """Test if we loaded something in the test content library folder."""
-    assert r.config.Interact().content_library_folder
+    assert r.Configuration().content_library_folder
