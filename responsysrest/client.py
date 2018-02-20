08abfecc7d4b4761fddb33c0c018f9a3d3e638b0
Dan Black <dyspop@gmail.com>
2018-02-20 14:37:36 -0500

    move gets inside Client

--- responsysrest/client.py
+++ responsysrest/client.py
@@ -79,41 +79,94 @@ class Client:
         response_object = json.loads(response.text)
         return response_object
 
+    """Direct implentations of calls from Responsys Interact REST API documentation
+    https://docs.oracle.com/cloud/latest/marketingcs_gs/OMCEB/OMCEB.pdf
+    All comment descriptions are directly from the v1.3 REST API documentation,
+    except some English-language grammar and syntax inconsistencies are
+    modified from their documentation and code-comment style to match PEP-8.
+    """
+
     def get_profile_lists(self):
         """Retrieving all profile lists for an account."""
         return self._get('lists')
 
+    def get_campaigns(self):
+        """Get all EMD email campaigns."""
+        return self._get('campaigns')
+
+    def get_push_campaigns(self):
+        """Get all Push campaigns."""
+        return self._get('campaigns?type=push')
+
+    def get_member_of_list_by_riid(
+        self,
+        list_name,
+        riid,
+        fields_to_return=['all']
+    ):
+        """Retrieve a member of a profile list using RIID."""
+        service_url = f'lists/{list_name}/members/{riid}'
+        parameters = f'fs={",".join(fields_to_return)}'
+        return self._get(service_url, parameters=parameters)
+
+    def get_member_of_list_by_attribute(
+        self,
+        list_name,
+        record_id,
+        query_attribute='c',
+        fields_to_return=['all']
+    ):
+        """Retrieve a member of a profile list based on query attribute."""
+        service_url = f'lists/{list_name}/members'
+        parameters = f'fs={",".join(fields_to_return)}&qa={query_attribute}&id={record_id}'
+        return self._get(service_url, parameters=parameters)
+
+    def get_profile_extensions_for_list(self, list_name):
+        """Retrieve all profile extensions of a profile list."""
+        return self._get(f'lists/{list_name}/listExtensions')
 
 
-def get(service_url, context, **kwargs):
-    """General purpose build for GET requests to Interact API."""
-    auth_token = context["authToken"]
-    endpoint = f'{context["endPoint"]}/{context["api_url"]}/{service_url}'
-    headers = kwargs.get('headers', {'Authorization': auth_token})
-    # use parameters if we got them
-    if "parameters" in kwargs:
-        parameters = kwargs.get('parameters', None)
-        endpoint = f'{endpoint}?{parameters}'
-    return json.loads(requests.get(url=endpoint, headers=headers).text)
+    def get_member_of_profile_extension_by_riid(
+        self,
+        list_name,
+        pet_name,
+        riid,
+        fields_to_return=['all']
+    ):
+        """Retrieve a member of a profile extension table based on RIID."""
+        service_url = f'lists/{list_name}/listExtensions/{pet_name}/members/{riid}'
+        parameters = f'fs={",".join(fields_to_return)}'
+        return self._get(service_url, parameters=parameters)
+
+    def get_member_of_profile_extension_by_attribute(
+        self,
+        list_name,
+        pet_name,
+        record_id,
+        query_attribute='c',
+        fields_to_return=['all']
+    ):
+        """Retrieve a member of a profile extension table based on a query attribute."""
+        service_url = f'lists/{list_name}/listExtensions/{pet_name}/members'
+        parameters = f'fs={",".join(fields_to_return)}&qa={query_attribute}&id={record_id}'
+        return self._get(service_url, parameters=parameters)
 
 
-"""Direct implentations of calls from Responsys Interact REST API documentation
-https://docs.oracle.com/cloud/latest/marketingcs_gs/OMCEB/OMCEB.pdf
-All comment descriptions are directly from the v1.3 REST API documentation,
-except some English-language grammar and syntax inconsistencies are modified
-from their documentation and code-comment style to match PEP-8.
-"""
+    def get_lists_for_record(self, riid):
+        """Find what lists a record is in by RIID."""
+        all_lists = [list_name["name"] for list_name in self.get_profile_lists()]
+        # container list
+        member_of = []
+        for profile_list in all_lists:
+            response = self.get_member_of_list_by_riid(
+                self, profile_list, riid)
+            # if the member (by riid) is in the profile list
+            # add it to the list of all profile lists
+            if "recordData" in response:
+                member_of.append(profile_list)
+        return member_of
 
 
-def login(user_name, password, url):
-    """Login with username and password."""
-    data = {
-        "user_name": user_name,
-        "password": password,
-        "auth_type": "password"
-    }
-    headers = {'content-type': 'application/x-www-form-urlencoded'}
-    return requests.post(url, data=data, headers=headers)
 
 
 # # TODO: Implement
@@ -163,21 +216,6 @@ def login(user_name, password, url):
 #     response = requests.post(url, data=data, headers=headers)
 #     return response
 
-
-def get_profile_lists(context):
-    """Retrieving all profile lists for an account."""
-    return get('lists', context)
-
-
-def get_campaigns(context):
-    """Get all EMD email campaigns."""
-    return get('campaigns', context)
-
-
-def get_push_campaigns(context):
-    """Get all Push campaigns."""
-    return get('campaigns?type=push', context)
-
 # TODO: implement context and api url with post
 def send_email_message(email_address, folder_name, campaign_name, context):
     """Trigger email message."""
@@ -205,104 +243,83 @@ def send_email_message(email_address, folder_name, campaign_name, context):
     return requests.post(data=json.dumps(data), headers=headers, url=url)
 
 
-# Merge or update members in a profile list table
-# TODO: fix 403 response
-# TODO: implement context and api_url with post
-def manage_profile_list(list_name, **kwargs):
-    """Merge or update members in a profile list table."""
-    # load container data
-    data = rules["merge_or_update_members_in_a_profile_list_table"][0]
-    # process keyword arguments
-    fields = kwargs.get('fields')
-    records = kwargs.get('records', None)
-    merge_rules = kwargs.get('merge_rules', data["mergeRule"])
+# # Merge or update members in a profile list table
+# # TODO: fix 403 response
+# # TODO: implement context and api_url with post
+# def manage_profile_list(list_name, **kwargs):
+#     """Merge or update members in a profile list table."""
+#     # load container data
+#     data = rules["merge_or_update_members_in_a_profile_list_table"][0]
+#     # process keyword arguments
+#     fields = kwargs.get('fields')
+#     records = kwargs.get('records', None)
+#     merge_rules = kwargs.get('merge_rules', data["mergeRule"])
 
-    # make sure the input fields and records are lists
-    if isinstance(fields, list) and isinstance(records, list):
-        # make sure the fields and records have the same amount of columns
-        if len(fields) == len(records):
-            # insert our fields into the data
-            data["recordData"]["fieldNames"] = fields
-            # insert our records into the data
-            data["recordData"]["records"] = records
-        else:
-            raise ValueError(
-                """ERROR: List headers count does not
-                match record column count"""
-            )
-    else:
-        raise ValueError(
-            """
-            ARGUMENT ERROR: input fields or records are not list objects.\n
-            Please specify lists for 'fields' or 'records' arguments.
-            """
-        )
-    # extract merge rules
-    rules_keys = [key for key in data["mergeRule"]]
-    # extract merge rules default values
-    rules_values = [
-        data["mergeRule"][rule]["default"] for rule in rules_keys
-    ]
-    # assign a new rules object to work on before we insert it into the
-    # request object
-    rules_dict = dict(zip(rules_keys, rules_values))
+#     # make sure the input fields and records are lists
+#     if isinstance(fields, list) and isinstance(records, list):
+#         # make sure the fields and records have the same amount of columns
+#         if len(fields) == len(records):
+#             # insert our fields into the data
+#             data["recordData"]["fieldNames"] = fields
+#             # insert our records into the data
+#             data["recordData"]["records"] = records
+#         else:
+#             raise ValueError(
+#                 """ERROR: List headers count does not
+#                 match record column count"""
+#             )
+#     else:
+#         raise ValueError(
+#             """
+#             ARGUMENT ERROR: input fields or records are not list objects.\n
+#             Please specify lists for 'fields' or 'records' arguments.
+#             """
+#         )
+#     # extract merge rules
+#     rules_keys = [key for key in data["mergeRule"]]
+#     # extract merge rules default values
+#     rules_values = [
+#         data["mergeRule"][rule]["default"] for rule in rules_keys
+#     ]
+#     # assign a new rules object to work on before we insert it into the
+#     # request object
+#     rules_dict = dict(zip(rules_keys, rules_values))
 
-    for merge_rule, merge_value in merge_rules.items():
-        try:
-            # if the user input merge rule value is valid based on the
-            # container data
-            if merge_value in data["mergeRule"][merge_rule]["options"]:
-                # add the new merge rule value to the data
-                data["mergeRule"][merge_rule] = merge_value
-        except KeyError:
-            print(
-                """
-                f'ERROR: Merge rule "{merge_rule}" is not valid.
-                Valid merge rules are:
-                {rules_keys}'
-                """
-            )
-        # print(merge_rule + " : " + merge_value)
-        # assign parameters from merge_rules keyword arguments to new rules
-        rules_dict[merge_rule] = merge_value
+#     for merge_rule, merge_value in merge_rules.items():
+#         try:
+#             # if the user input merge rule value is valid based on the
+#             # container data
+#             if merge_value in data["mergeRule"][merge_rule]["options"]:
+#                 # add the new merge rule value to the data
+#                 data["mergeRule"][merge_rule] = merge_value
+#         except KeyError:
+#             print(
+#                 """
+#                 f'ERROR: Merge rule "{merge_rule}" is not valid.
+#                 Valid merge rules are:
+#                 {rules_keys}'
+#                 """
+#             )
+#         # print(merge_rule + " : " + merge_value)
+#         # assign parameters from merge_rules keyword arguments to new rules
+#         rules_dict[merge_rule] = merge_value
 
-    # add the merge rules back into the data
-    data["mergeRule"] = rules_dict
+#     # add the merge rules back into the data
+#     data["mergeRule"] = rules_dict
 
-    # build post request
-    context = get_context()
-    auth_token = context["authToken"]
-    url = f'{context["endPoint"]}/{api_url}/lists/{list_name}/members'
-    headers = {
-        'Authorization': auth_token, 'Content-Type': 'application/json'
-    }
-    print(json.dumps(data))
-    # make the request
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-    # return the data to the container?
-    data = rules["merge_or_update_members_in_a_profile_list_table"][0]
-    return response
-
-
-def get_member_of_list_by_riid(list_name, riid, context):
-    """Retrieve a member of a profile list using RIID."""
-    service_url = f'lists/{list_name}/members/{riid}'
-    # only support returning all fields for now
-    # TODO: implement other fields
-    return get(service_url, context, parameters='fs=all')
-
-
-def get_member_of_list_by_attribute(
-    list_name,
-    record_id,
-    context,
-    query_attribute='c',
-    fields_to_return='all'
-):
-    """Retrieve a member of a profile list based on query attribute."""
-    service_url = f'lists/{list_name}/members'
-    parameters = f'fs={fields_to_return}&qa={query_attribute}&id={record_id}'
-    return get(service_url, context, parameters=parameters)
+#     # build post request
+#     context = get_context()
+#     auth_token = context["authToken"]
+#     url = f'{context["endPoint"]}/{api_url}/lists/{list_name}/members'
+#     headers = {
+#         'Authorization': auth_token, 'Content-Type': 'application/json'
+#     }
+#     print(json.dumps(data))
+#     # make the request
+#     response = requests.post(url, data=json.dumps(data), headers=headers)
+#     # return the data to the container?
+#     data = rules["merge_or_update_members_in_a_profile_list_table"][0]
+#     return response
 
 # TODO implement context, api_url with delete
 def delete_from_profile_list(list_name, riid, context):
@@ -310,11 +327,6 @@ def delete_from_profile_list(list_name, riid, context):
     url = f'{context["endPoint"]}/{context["api_url"]}/lists/{list_name}/members/{riid}'
     headers = {'Authorization': context["authToken"]}
     return requests.delete(url=url, headers=headers)
-
-
-def get_profile_extensions(list_name, context):
-    """Retrieve all profile extensions of a profile list."""
-    return get(f'lists/{list_name}/listExtensions', context)
 
 # TODO implement context, api_url with post
 def create_profile_extension(
@@ -356,38 +368,6 @@ def create_profile_extension(
     # return merge_or_update_members_in_a_profile_extension_table()
 
 
-def get_member_of_profile_extension_by_riid(
-    list_name,
-    pet_name,
-    riid,
-    context,
-    fields_to_return='all'
-):
-    """Retrieve a member of a profile extension table based on RIID."""
-    return get(
-        f'lists/{list_name}/listExtensions/{pet_name}/members/{riid}',
-        context,
-        parameters=f'fs={fields_to_return}'
-    )
-
-
-def get_member_of_profile_extension_by_attribute(
-    list_name,
-    pet_name,
-    record_id,
-    context,
-    query_attribute='c',
-    fields_to_return='all'
-):
-    """Retrieve a member of a profile extension table
-    based on a query attribute.
-    """
-    service_url = f'lists/{list_name}/listExtensions/{pet_name}/members'
-    return get(
-        service_url,
-        context,
-        parameters=f'fs={fields_to_return}&qa={query_attribute}&id={record_id}'
-    )
 
 # TODO implement context, api_url with delete
 def delete_member_of_profile_extension_by_riid(
@@ -456,22 +436,3 @@ def delete_folder(context, folder_path=config.api_folder):
     url = f'{context["endPoint"]}/{context["api_url"]}/clFolders/contentlibrary/{folder_path}'
     return requests.delete(url=url, headers=headers)
 
-
-##################
-# Extra features #
-##################
-
-
-def get_lists_for_record(riid, context):
-    """Find what lists a record is in by RIID."""
-    all_lists = [list_name["name"] for list_name in get_profile_lists(context)]
-    # container list
-    member_of = []
-    for profile_list in all_lists:
-        response = get_member_of_list_by_riid(
-            profile_list, riid, context)
-        # if the member (by riid) is in the profile list
-        # add it to the list of all profile lists
-        if "recordData" in response:
-            member_of.append(profile_list)
-    return member_of
