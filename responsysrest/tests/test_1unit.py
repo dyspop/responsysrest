2005e9fd4e5a7f51144d9a8877be64dd02734081
Dan Black <dyspop@gmail.com>
2019-03-11 10:00:18 -0400

    add test for credentials with args

--- responsysrest/tests/test_1unit.py
+++ responsysrest/tests/test_1unit.py
@@ -6,3 +6,11 @@ def test_credentials_no_args():
     # Test that the credentials class type errors when no arguments provided.
     with pytest.raises(Exception) as e_info:
         credentials = r.Credentials()
+
+
+def test_credentials_with_args():
+    # Test that the credentials class instantiates with args
+    user_name = 'test_user_name'
+    password = 'test_pass_word'
+    email_address = 'email_address@email_address.biz'
+    assert r.Credentials(user_name, password, email_address)
